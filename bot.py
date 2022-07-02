315a01fa60a0ede766dd6ce0865f12bc6c99c883
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-02 18:06:23 +0200

    Add basic "Add RSS Feed" functionality to the bot
    Currently adding new feeds just adds them to a MongoDB.
    Every conversation (specifically channel ID) is a separate collection within the DB.
    Currently only feed link and custom feed name is stored.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-from os import getenv
-from dotenv import load_dotenv
 from discord.ext.commands import Bot, when_mentioned_or
-
+from dotenv import load_dotenv
+from os import getenv
+from rss_db import add_rss_to_db
 
 bot = Bot(command_prefix=when_mentioned_or("!"))
 
@@ -15,6 +15,16 @@ async def on_ready():
 @bot.command()
 async def hello(context):
     await context.send("Hello there!")
+
+
+@bot.command(aliases=["add"])
+async def add_feed(context, rss_feed=None, rss_name=None):
+    if not rss_feed or not rss_name:
+        await context.send_help(add_feed)
+        return
+    channel_id = context.channel.id
+    print(f"Adding RSS feed, channel_id=[{channel_id}] name=[{rss_name}] feed=[{rss_feed}]")
+    add_rss_to_db(channel_id, rss_feed, rss_name)
 
 
 load_dotenv()
