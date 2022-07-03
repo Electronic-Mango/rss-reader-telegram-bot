37954fb3804dfb00e89893a0930f08b6c4989712
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-03 18:27:41 +0200

    Improve hello command
    Respond with a random greeting from a list.

--- commands/hello.py
+++ commands/hello.py
@@ -1,9 +1,18 @@
 from logging import info
+from random import choice
 
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
 HELLO_HELP_MESSAGE = "/hello - say hello to the bot"
+HELLO_RESPONSES = [
+    "Hello there!",
+    "Hello!",
+    "Hi!",
+    "Greetings!",
+    "Howdy!",
+    "Hey!",
+]
 
 
 def hello_command_handler():
@@ -12,4 +21,4 @@ def hello_command_handler():
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     info(f"Hello from chat ID: [{update.effective_chat.id}]")
-    await update.message.reply_text("Hello there!")
+    await update.message.reply_text(choice(HELLO_RESPONSES))
