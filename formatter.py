e35d6cf127834b162829e3ace4e5c5d8109150a1
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-05 23:39:16 +0200

    Improve message fromatting and media URL extraction
    Use HTML parser and unescape for both message content and media URLs.

--- formatter.py
+++ formatter.py
@@ -1,17 +1,19 @@
+from html import unescape
 from re import findall, match, sub
 
 
 def parse_summary(entry):
     summary = entry.summary
+    summary = unescape(summary)
     summary = sub(r"(<a.+</a>)+", "", summary)
     summary = sub(r"(<video controls>.+</video>)+", "", summary)
-    summary = summary.replace("&quot;", '"')
     summary = sub(r"<br( )?(/)?>", "\n", summary)
     summary = sub(r"<p>", "", summary)
     summary = sub(r"</p>", "\n", summary)
     summary = sub(r"<img.+/>", "", summary)
     summary = summary.replace("(no text)", "")
     summary = "\n".join([line.strip() for line in summary.strip().splitlines() if line.strip()])
+    print(summary)
     return summary
 
 
@@ -22,4 +24,4 @@ def parse_media(entry):
     img_pattern = r"<img.*? src=\"(.*?)\""
     match(img_pattern, summary)
     img_urls = findall(img_pattern, summary)
-    return [(url, None) for url in img_urls]
+    return [(unescape(url), None) for url in img_urls]
