7f37c5d0a6cc7f4d39634d685034effd9c2e1469
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-10 20:26:59 +0200

    Remove TODO related to parsing text out of links, since it's no longer relevant

--- feed_parser.py
+++ feed_parser.py
@@ -10,10 +10,6 @@ def parse_entry(entry: FeedParserDict) -> tuple[str, str, list[str]]:
 def _parse_summary(entry: FeedParserDict) -> str:
     summary = BeautifulSoup(entry["summary"], "html.parser")
     all_text = summary.find_all(text=True)
-    # TODO Handle links in some smarter way.
-    # Perhaps appending them with "link: " would be better?
-    # Right now some text in the form of a link will get removed.
-    # Although appending "link" anywhere seem more like bot-related approach.
     return "".join(text for text in all_text).strip()
 
 
