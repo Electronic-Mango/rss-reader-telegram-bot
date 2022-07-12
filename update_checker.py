def125cd931791db4cb00b64513ecb92be211446
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-12 21:19:07 +0200

    Check for updates for each feed in a separate, staggered job
    Checking for all updates in a single job froze bot for it's duration and stopped responding for commands.
    Now each feed is checked in a separate job.
    In order to further prevent bot freezing there's new environment variable LOOKUP_FEED_DELAY_SECONDS which set how many seconds are there between each check.
    Also, both update check interval and initial delays are changed to floats.

--- update_checker.py
+++ update_checker.py
@@ -8,29 +8,29 @@ from db import get_all_stored_data, update_stored_latest_id
 from feed_parser import parse_entry
 from feed_reader import get_not_handled_entries
 from sender import send_update
+from settings import LOOKUP_FEED_DELAY_SECONDS
 
 _logger = getLogger(__name__)
 
 
 async def check_for_all_updates(context: ContextTypes.DEFAULT_TYPE) -> None:
     _logger.info("Starting checking for all updates")
-    for chat_id, feed_type, feed_name, latest_id in get_all_stored_data():
-        await _check_for_updates(context.bot, chat_id, feed_type, feed_name, latest_id)
+    for delay, feed_data in enumerate(get_all_stored_data()):
+        context.job_queue.run_once(
+            callback=_check_for_updates,
+            when=delay * LOOKUP_FEED_DELAY_SECONDS,
+            data=feed_data
+        )
 
 
-async def _check_for_updates(
-    bot: Bot,
-    chat_id: int,
-    feed_type: str,
-    feed_name: str,
-    latest_id: str,
-) -> None:
+async def _check_for_updates(context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id, feed_type, feed_name, latest_id = context.job.data
     _logger.info(f"[{chat_id}] Checking for updates for [{feed_name}] [{feed_type}]")
     not_handled_feed_entries = get_not_handled_entries(feed_type, feed_name, latest_id)
     if not not_handled_feed_entries:
         _logger.info(f"[{chat_id}] No new data for [{feed_name}] [{feed_type}]")
     else:
-        await _handle_update(bot, chat_id, feed_type, feed_name, not_handled_feed_entries)
+        await _handle_update(context.bot, chat_id, feed_type, feed_name, not_handled_feed_entries)
 
 
 async def _handle_update(
