117b776cdb0e71d98a762912755759cfbbf2a4c8
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-13 22:09:30 +0200

    Move not bot-related logic from bot module to main
    Move configuration of logging and initialization of DB from bot module to main.

--- bot/bot.py
+++ bot/bot.py
@@ -3,12 +3,9 @@
 # Perhaps a DataClass would be better? At least for FeedMedia, to avoid complex return types.
 # TODO Perhaps add a "get" command, which will just dump a feed to a user
 
-from logging import INFO, StreamHandler, basicConfig, getLogger
-from logging.handlers import RotatingFileHandler
-from sys import stdout
+from logging import getLogger
 
-from db import initialize_db
-from settings import BACKUP_COUNT, LOG_PATH, MAX_BYTES, TOKEN
+from settings import TOKEN
 from telegram.ext import Application, ApplicationBuilder, Defaults
 
 from bot.commands.add import add_conversation_handler
@@ -29,34 +26,17 @@ _UPDATE_HANDLERS = [
     remove_all_conversation_handler(),
 ]
 
-_logger = getLogger("bot.main")
+_logger = getLogger(__name__)
 
 
-def _main() -> None:
-    _configure_logging()
-    initialize_db()
+def run_bot() -> None:
     application = ApplicationBuilder().token(TOKEN).defaults(Defaults("HTML")).build()
     _configure_handlers(application)
     start_checking_for_updates(application.job_queue)
     application.run_polling()
 
 
-def _configure_logging() -> None:
-    basicConfig(
-        format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
-        level=INFO,
-        handlers=[
-            StreamHandler(stdout),
-            RotatingFileHandler(LOG_PATH, maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT),
-        ],
-    )
-
-
 def _configure_handlers(application: Application) -> None:
     _logger.info("Configuring handlers...")
     application.add_handlers(_UPDATE_HANDLERS)
     application.add_error_handler(handle_errors)
-
-
-if __name__ == "__main__":
-    _main()
--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-from bot.bot import _main
+from logging import INFO, StreamHandler, basicConfig
+from logging.handlers import RotatingFileHandler
+from sys import stdout
+
+from bot.bot import run_bot
+from db import initialize_db
+from settings import BACKUP_COUNT, LOG_PATH, MAX_BYTES
+
+
+def _main() -> None:
+    _configure_logging()
+    initialize_db()
+    run_bot()
+
+
+def _configure_logging() -> None:
+    basicConfig(
+        format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
+        level=INFO,
+        handlers=[
+            StreamHandler(stdout),
+            RotatingFileHandler(LOG_PATH, maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT),
+        ],
+    )
 
 
 if __name__ == "__main__":
