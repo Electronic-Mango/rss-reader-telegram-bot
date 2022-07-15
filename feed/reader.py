4f35fe9c0b10121f83d1d295de24143b5510194c
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-15 22:24:26 +0200

    Check if feed is still valid when checking for updates
    As of now information about no longer valid feeds will only be printed to logs, information won't be send back to the user.

--- feed/reader.py
+++ feed/reader.py
@@ -27,23 +27,28 @@ def get_not_handled_entries(feed_type: str, feed_name: str, target_id: str) -> l
 
 def feed_is_valid(feed_type: str, feed_name: str) -> bool:
     _logger.info(f"Checking if [{feed_name}] [{feed_type}] feed exists")
-    # 301 is a workaround for tumblr blogs with dedicated URLs
-    return _get_parsed_feed(feed_type, feed_name)["status"] in [200, 301]
+    parsed_feed = _get_parsed_feed(feed_type, feed_name)
+    return _feed_is_valid(parsed_feed)
 
 
 def _get_sorted_entries(feed_type: str, feed_name: str) -> list[FeedParserDict]:
-    entries = _get_parsed_feed(feed_type, feed_name)["entries"]
+    parsed_feed = _get_parsed_feed(feed_type, feed_name)
+    if not _feed_is_valid(parsed_feed):
+        _logger.error(f"Feed for [{feed_name}] [{feed_type}] is not valid anymore")
+        return []
+    entries = parsed_feed["entries"]
     return sorted(entries, key=lambda entry: parse_date(entry["published"]), reverse=True)
 
 
 def _get_parsed_feed(feed_type: str, feed_name: str) -> FeedParserDict:
-    feed_link = _get_feed_link(feed_type, feed_name)
+    feed_link = RSS_FEEDS[feed_type].format(source_pattern=feed_name)
     _logger.info(f"Parsed [{feed_name}][{feed_type}] to link [{feed_link}]")
     return parse(feed_link)
 
 
-def _get_feed_link(feed_type: str, feed_name: str) -> str:
-    return RSS_FEEDS[feed_type].format(source_pattern=feed_name)
+def _feed_is_valid(feed: FeedParserDict) -> bool:
+    # 301 is a workaround for tumblr blogs with dedicated URLs
+    return feed["status"] in [200, 301]
 
 
 def _not_latest_entry(target_id: str, entry_id: str) -> bool:
