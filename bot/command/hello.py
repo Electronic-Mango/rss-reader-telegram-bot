e11f8e4e73d491c923a1c5db96c65fae3d439700
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-16 18:45:36 +0200

    Simplify "hello" command response

--- bot/command/hello.py
+++ bot/command/hello.py
@@ -1,20 +1,9 @@
 from logging import getLogger
-from random import choice
 
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
 HELLO_HELP_MESSAGE = "/hello - say hello to the bot"
-
-# TODO Extract to file?
-_HELLO_RESPONSES = [
-    "Hello there!",
-    "Hello!",
-    "Hi!",
-    "Greetings!",
-    "Howdy!",
-    "Hey!",
-]
 
 _logger = getLogger(__name__)
 
@@ -25,4 +14,4 @@ def hello_command_handler() -> CommandHandler:
 
 async def _hello(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     _logger.info(f"[{update.effective_chat.id}] Hello!")
-    await update.message.reply_text(choice(_HELLO_RESPONSES))
+    await update.message.reply_text("Hello there!")
