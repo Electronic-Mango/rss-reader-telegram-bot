b0a7ac64e17afa3df3b3ab9480550f4538c97b4b
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-20 22:30:54 +0200

    Allow going back to subscriptions in "remove" command
    After user select subscription to remove they will now be able to go back to the list of all subscriptions without restarting the command.

--- src/main/bot/command/remove.py
+++ src/main/bot/command/remove.py
@@ -3,6 +3,7 @@ Module handling the "remove" command, allowing users to remove a single subscrip
 """
 
 from logging import getLogger
+from typing import Callable
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Message, Update
 from telegram.ext import CallbackQueryHandler, CommandHandler, ContextTypes, ConversationHandler
@@ -15,6 +16,7 @@ REMOVE_HELP_MESSAGE = "/remove - remove subscription for a given feed"
 _REMOVE_FEED, _CONFIRM = range(2)
 _CONFIRM_REMOVAL_YES = "Yes"
 _CONFIRM_REMOVAL_NO = "No"
+_GO_BACK = "« Back to subscriptions"
 
 _logger = getLogger(__name__)
 
@@ -27,6 +29,7 @@ def remove_conversation_handler() -> ConversationHandler:
             _CONFIRM: [
                 CallbackQueryHandler(_remove_subscription, lambda data: isinstance(data, tuple)),
                 CallbackQueryHandler(_cancel, f"^{_CONFIRM_REMOVAL_NO}$"),
+                CallbackQueryHandler(_handle_go_back_to_subscription_list, f"^{_GO_BACK}$"),
             ],
         },
         fallbacks=[CommandHandler("remove", _handle_remove_request, USER_FILTER)],
@@ -45,17 +48,25 @@ async def _handle_remove_request(update: Update, _: ContextTypes.DEFAULT_TYPE) -
     chat_id = update.effective_chat.id
     _logger.info(f"[{chat_id}] User requested removal of subscription")
     if chat_has_stored_feeds(chat_id):
-        return await _request_feed_to_remove(update.message, chat_id)
+        return await _request_feed_to_remove(update.message.reply_text, chat_id)
     else:
         return await _no_feeds_to_remove(update.message, chat_id)
 
 
-async def _request_feed_to_remove(message: Message, chat_id: int) -> int:
+async def _handle_go_back_to_subscription_list(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+    chat_id = update.effective_chat.id
+    _logger.info(f"[{chat_id}] User went back to subscriptions list")
+    return await _request_feed_to_remove(query.edit_message_text, chat_id)
+
+
+async def _request_feed_to_remove(response_callback: Callable, chat_id: int) -> int:
     keyboard = [
         [InlineKeyboardButton(f"{feed_name} ({feed_type})", callback_data=(feed_name, feed_type))]
         for feed_type, feed_name in get_stored_feed_type_and_name(chat_id)
     ]
-    await message.reply_text(
+    await response_callback(
         "Select feed to remove",
         reply_markup=InlineKeyboardMarkup(keyboard),
     )
@@ -74,12 +85,15 @@ async def _request_confirmation(update: Update, _: ContextTypes.DEFAULT_TYPE) ->
     feed_name, feed_type = query.data
     _logger.info(f"[{update.effective_chat.id}] Selected [{feed_name}] [{feed_type}] for removal")
     keyboard = [
-        InlineKeyboardButton(_CONFIRM_REMOVAL_YES, callback_data=(feed_name, feed_type)),
-        InlineKeyboardButton(_CONFIRM_REMOVAL_NO, callback_data=_CONFIRM_REMOVAL_NO),
+        [
+            InlineKeyboardButton(_CONFIRM_REMOVAL_YES, callback_data=(feed_name, feed_type)),
+            InlineKeyboardButton(_CONFIRM_REMOVAL_NO, callback_data=_CONFIRM_REMOVAL_NO),
+        ],
+        [InlineKeyboardButton(_GO_BACK, callback_data=_GO_BACK)],
     ]
     await query.edit_message_text(
         f"Confirm removal of <b>{feed_name}</b> ({feed_type})",
-        reply_markup=InlineKeyboardMarkup([keyboard]),
+        reply_markup=InlineKeyboardMarkup(keyboard),
     )
     return _CONFIRM
 
