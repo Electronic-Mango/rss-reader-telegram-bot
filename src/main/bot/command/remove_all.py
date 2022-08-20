3e6144b6e42ca9603821f5a8be757a79e0b5cb5a
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-20 21:11:01 +0200

    Migrate "removeall" command to use inline keyboard

--- src/main/bot/command/remove_all.py
+++ src/main/bot/command/remove_all.py
@@ -4,9 +4,8 @@ Module handling the "removeall" command, allowing users delete all subscriptions
 
 from logging import getLogger
 
-from telegram import Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import CommandHandler, ContextTypes, ConversationHandler, MessageHandler
-from telegram.ext.filters import COMMAND, TEXT
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Message, Update
+from telegram.ext import CallbackQueryHandler, CommandHandler, ContextTypes, ConversationHandler
 
 from bot.user_filter import USER_FILTER
 from db.wrapper import chat_has_stored_feeds, remove_stored_chat_data
@@ -15,7 +14,9 @@ REMOVE_ALL_HELP_MESSAGE = "/removeall - remove all subscriptions"
 
 _CONFIRM_1, _CONFIRM_2 = range(2)
 _CONFIRM_1_YES = "Yes"
+_CONFIRM_1_NO = "No"
 _CONFIRM_2_YES = "Yes, I'm sure"
+_CONFIRM_2_NO = "No, don't remove"
 
 _logger = getLogger(__name__)
 
@@ -24,18 +25,24 @@ def remove_all_conversation_handler() -> ConversationHandler:
     return ConversationHandler(
         entry_points=[CommandHandler("removeall", _request_confirmation_1, USER_FILTER)],
         states={
-            _CONFIRM_1: [MessageHandler(USER_FILTER & TEXT & ~COMMAND, _request_confirmation_2)],
-            _CONFIRM_2: [MessageHandler(USER_FILTER & TEXT & ~COMMAND, _remove_all)],
+            _CONFIRM_1: [
+                CallbackQueryHandler(_request_confirmation_2, f"^{_CONFIRM_1_YES}$"),
+                CallbackQueryHandler(_cancel, f"^{_CONFIRM_1_NO}$"),
+            ],
+            _CONFIRM_2: [
+                CallbackQueryHandler(_remove_all_subscriptions, f"^{_CONFIRM_2_YES}$"),
+                CallbackQueryHandler(_cancel, f"^{_CONFIRM_2_NO}$"),
+            ],
         },
-        fallbacks=[CommandHandler("cancel", _cancel, USER_FILTER)],
+        fallbacks=[CommandHandler("removeall", _request_confirmation_1, USER_FILTER)],
     )
 
 
 async def _cancel(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
     _logger.info(f"[{update.effective_chat.id}] User cancelled removing subscriptions")
-    await update.message.reply_text(
-        "Cancelled removing all subscriptions", reply_markup=ReplyKeyboardRemove()
-    )
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text("Cancelled removing all subscriptions")
     return ConversationHandler.END
 
 
@@ -43,18 +50,32 @@ async def _request_confirmation_1(update: Update, _: ContextTypes.DEFAULT_TYPE) 
     chat_id = update.effective_chat.id
     _logger.info(f"[{chat_id}] User requested removal of all subscriptions")
     if not chat_has_stored_feeds(chat_id):
-        await _no_feeds_to_remove(update.message, chat_id)
-        return
+        return await _no_feeds_to_remove(update.message, chat_id)
     await update.message.reply_text(
         "Confirm removal of all feeds",
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[[_CONFIRM_1_YES, "No"]],
-            one_time_keyboard=True,
-            resize_keyboard=True,
-            input_field_placeholder="Confirm removal",
-        ),
+        reply_markup=_prepare_keyboard(_CONFIRM_1_YES, _CONFIRM_1_NO),
     )
     return _CONFIRM_1
+
+
+async def _request_confirmation_2(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(
+        "Are you sure you want to remove <b>all</b> subscriptions?",
+        reply_markup=_prepare_keyboard(_CONFIRM_2_NO, _CONFIRM_2_YES),
+    )
+    return _CONFIRM_2
+
+
+async def _remove_all_subscriptions(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+    chat_id = update.effective_chat.id
+    _logger.info(f"[{chat_id}] Removing all subscriptions")
+    remove_stored_chat_data(chat_id)
+    await query.edit_message_text("Removed all subscriptions")
+    return ConversationHandler.END
 
 
 async def _no_feeds_to_remove(message: Message, chat_id: int) -> int:
@@ -63,29 +84,6 @@ async def _no_feeds_to_remove(message: Message, chat_id: int) -> int:
     return ConversationHandler.END
 
 
-async def _request_confirmation_2(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    confirmation_1 = update.message.text
-    if confirmation_1 != _CONFIRM_1_YES:
-        return await _cancel(update, context)
-    _logger.info(f"[{update.effective_chat.id}] User send first confirmation")
-    await update.message.reply_text(
-        "Are you sure you want to remove <b>all</b> subscriptions?",
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[["No, don't remove", _CONFIRM_2_YES]],
-            one_time_keyboard=True,
-            resize_keyboard=True,
-            input_field_placeholder="Confirm removal",
-        ),
-    )
-    return _CONFIRM_2
-
-
-async def _remove_all(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    confirmation_2 = update.message.text
-    if confirmation_2 != _CONFIRM_2_YES:
-        return await _cancel(update, context)
-    chat_id = update.effective_chat.id
-    _logger.info(f"[{chat_id}] Removing all subscriptions")
-    remove_stored_chat_data(chat_id)
-    await update.message.reply_text("Removed all subscriptions")
-    return ConversationHandler.END
+def _prepare_keyboard(*data: str) -> InlineKeyboardMarkup:
+    keyboard = map(lambda data: InlineKeyboardButton(data, callback_data=data), data)
+    return InlineKeyboardMarkup([list(keyboard)])
