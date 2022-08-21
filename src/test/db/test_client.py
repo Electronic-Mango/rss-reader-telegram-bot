d78831d9b3f81b83b254e909074af99dfe0a6255
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-21 15:34:40 +0200

    Unify DB manipulation unit tests
    Create one parametrized unit tests for testing all functions manipulating the database.

--- src/test/db/test_client.py
+++ src/test/db/test_client.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock, patch
 
 from pymongo import ASCENDING
+from pytest import mark
 
 from db.client import delete_many, exists, find_many, initialize_db, insert_one, update_one
 from settings import DB_COLLECTION_NAME, DB_HOST, DB_NAME, DB_PORT
@@ -31,45 +32,21 @@ def test_initialize_db(mongo_client_mock: MagicMock) -> None:
 
 
 @patch("db.client.MongoClient", side_effect=mocked_mongo_client)
-def test_insert_one(_) -> None:
+@mark.parametrize(
+    argnames=["client_function", "db_function", "args"],
+    argvalues=[
+        (insert_one, "insert_one", (document,)),
+        (delete_many, "delete_many", (filter,)),
+        (update_one, "find_one_and_update", (filter, document)),
+        (find_many, "find", (filter,)),
+        (exists, "count_documents", (filter,)),
+    ],
+)
+def test_db_operations(_, client_function, db_function, args) -> None:
     initialize_db()
-    collection_mock.insert_one.return_value = operation_result_mock
-    insertion_result = insert_one(document)
+    db_function = getattr(collection_mock, db_function)
+    db_function.return_value = operation_result_mock
+    insertion_result = client_function(*args)
+    assert db_function.call_count
     assert operation_result_mock == insertion_result
-    assert (document,) == collection_mock.insert_one.call_args.args
-
-
-@patch("db.client.MongoClient", side_effect=mocked_mongo_client)
-def test_delete_many(_) -> None:
-    initialize_db()
-    collection_mock.delete_many.return_value = operation_result_mock
-    insertion_result = delete_many(filter)
-    assert operation_result_mock == insertion_result
-    assert (filter,) == collection_mock.delete_many.call_args.args
-
-
-@patch("db.client.MongoClient", side_effect=mocked_mongo_client)
-def test_update_one(_) -> None:
-    initialize_db()
-    collection_mock.find_one_and_update.return_value = operation_result_mock
-    insertion_result = update_one(filter, document)
-    assert operation_result_mock == insertion_result
-    assert (filter, document) == collection_mock.find_one_and_update.call_args.args
-
-
-@patch("db.client.MongoClient", side_effect=mocked_mongo_client)
-def test_find_many(_) -> None:
-    initialize_db()
-    collection_mock.find.return_value = operation_result_mock
-    insertion_result = find_many(filter)
-    assert operation_result_mock == insertion_result
-    assert (filter,) == collection_mock.find.call_args.args
-
-
-@patch("db.client.MongoClient", side_effect=mocked_mongo_client)
-def test_exists(_) -> None:
-    initialize_db()
-    collection_mock.count_documents.return_value = operation_result_mock
-    insertion_result = exists(filter)
-    assert operation_result_mock == insertion_result
-    assert (filter,) == collection_mock.count_documents.call_args.args
+    assert args == db_function.call_args.args
