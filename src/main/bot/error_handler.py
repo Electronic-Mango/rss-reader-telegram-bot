0cc44b51af7a8cb6c2cdf51e433f1c0059bf4f66
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-09-29 17:21:09 +0200

    Send info to chat about errors only when chat ID is available

--- src/main/bot/error_handler.py
+++ src/main/bot/error_handler.py
@@ -23,8 +23,8 @@ async def handle_errors(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         return
     chat_id = update.effective_chat.id if update is not None else context.job.chat_id
     _logger.warn(f"[{chat_id}] Error when handling update:", exc_info=error)
-    if type(error) is Forbidden:
+    if type(error) is Forbidden and chat_id:
         _logger.warn(f"[{chat_id}] Cannot send updates to chat, removing chat data")
         remove_stored_chat_data(chat_id)
-    else:
+    elif chat_id:
         await context.bot.send_message(chat_id, f"Error when handling an update:\n{error}")
