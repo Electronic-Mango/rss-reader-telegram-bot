63ac81e78225c2fe93a67f83824a9e7815f98a79
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-06-15 12:52:28 +0200

    Use correct function for loguru warnings

--- src/main/bot/error_handler.py
+++ src/main/bot/error_handler.py
@@ -26,7 +26,7 @@ async def handle_errors(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 async def _handle_update_error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     error = context.error
-    logger.warn(f"[{chat_id}] Error when handling update:", exc_info=error)
+    logger.warning(f"[{chat_id}] Error when handling update:", exc_info=error)
     if type(error) is Forbidden and chat_id:
         await _handle_forbidden_error(chat_id)
     elif chat_id:
@@ -37,15 +37,15 @@ async def _handle_job_error(context: ContextTypes.DEFAULT_TYPE) -> None:
     error = context.error
     chat_id, feed_type, feed_name, link, title, description = context.job.data
     context.job.data = chat_id
-    logger.warn(f"[{chat_id}] Error in job:", exc_info=error)
+    logger.warning(f"[{chat_id}] Error in job:", exc_info=error)
     if type(error) is Forbidden:
         await _handle_forbidden_error(chat_id)
     else:
-        logger.warn(f"[{chat_id}] Trying to resend data without media")
+        logger.warning(f"[{chat_id}] Trying to resend data without media")
         description = f"<b>Error when sending original update: {error}</b>\n\n{description}"
         await send_update(context.bot, chat_id, feed_type, feed_name, link, title, description)
 
 
 async def _handle_forbidden_error(chat_id: int) -> None:
-    logger.warn(f"[{chat_id}] Cannot send updates to chat, removing chat data")
+    logger.warning(f"[{chat_id}] Cannot send updates to chat, removing chat data")
     remove_stored_chat_data(chat_id)
