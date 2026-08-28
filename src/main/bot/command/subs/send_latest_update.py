22581598410c2cafe16091368cd1d0c79107015e
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-08-28 19:02:01 +0200

    Revert updating message ID through details

--- src/main/bot/command/subs/send_latest_update.py
+++ src/main/bot/command/subs/send_latest_update.py
@@ -1,11 +1,10 @@
-from feedparser.util import FeedParserDict
 from loguru import logger
 from telegram import Bot, Update
 from telegram.ext import ContextTypes
 
 from bot.command.subs.conversation_state import ConversationState
 from bot.sender import send_update
-from db.wrapper import get_latest_message_id, update_latest_message_id
+from db.wrapper import get_latest_message_id
 from feed.parser import parse_description, parse_link, parse_media_links, parse_title
 from feed.reader import get_parsed_feed, get_sorted_entries
 
@@ -13,27 +12,24 @@ from feed.reader import get_parsed_feed, get_sorted_entries
 async def send_latest_update(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> ConversationState:
-    """Send latest entry for selected subscription, mostly meant for debugging purposes"""
+    """Send latest entry for selected subscription, mostly meant for debugging purposes."""
     query = update.callback_query
     await query.answer()
     bot = context.bot
     chat_id = update.effective_chat.id
     type, name, _ = query.data
-    logger.info(f"[{chat_id}] Resending latest update for [{type}] [{name}]")
-    entries = get_sorted_entries(get_parsed_feed(type, name))
-    if not entries:
-        logger.warning(f"[{chat_id}] No entries found for [{type}] [{name}]")
-        await bot.send_message(chat_id, f"No entries found for <b>{name}</b>!")
-        return ConversationState.SHOW_DETAILS
-    await _prepare_and_send_update(entries, bot, chat_id, type, name)
+    await _handle_update(bot, chat_id, type, name)
     return ConversationState.SHOW_DETAILS
 
 
-async def _prepare_and_send_update(
-    entries: list[FeedParserDict], bot: Bot, chat_id: int, type: str, name: str
-) -> None:
+async def _handle_update(bot: Bot, chat_id: int, type: str, name: str) -> None:
+    if not (entries := get_sorted_entries(get_parsed_feed(type, name))):
+        logger.warning(f"[{chat_id}] No entries found for [{type}] [{name}]")
+        await bot.send_message(chat_id, f"No entries found for <b>{name}</b>!")
+        return
+    logger.info(f"[{chat_id}] Resending latest update for [{type}] [{name}]")
     latest_entry = entries[0]
-    latest_message_id = await send_update(
+    await send_update(
         bot,
         chat_id,
         type,
@@ -44,4 +40,3 @@ async def _prepare_and_send_update(
         get_latest_message_id(chat_id, type, name),
         parse_media_links(latest_entry),
     )
-    update_latest_message_id(chat_id, type, name, latest_message_id)
