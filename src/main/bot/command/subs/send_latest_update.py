338bbf73ac710216f2ff52daa77224d5242ffe9b
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-08-28 18:48:28 +0200

    Temporarily update latest message ID in the DB
    This is only for easier testing if the logic of latest message IDs works correctly.

--- src/main/bot/command/subs/send_latest_update.py
+++ src/main/bot/command/subs/send_latest_update.py
@@ -4,7 +4,7 @@ from telegram.ext import ContextTypes
 
 from bot.command.subs.conversation_state import ConversationState
 from bot.sender import send_update
-from db.wrapper import get_latest_message_id
+from db.wrapper import get_latest_message_id, update_latest_message_id
 from feed.parser import parse_description, parse_link, parse_media_links, parse_title
 from feed.reader import FeedParsedEntry, get_parsed_feed, get_sorted_entries
 
@@ -32,9 +32,15 @@ async def _prepare_and_send_update(
     entries: list[FeedParsedEntry], bot: Bot, chat_id: int, type: str, name: str
 ) -> None:
     latest_entry = entries[0]
-    link = parse_link(latest_entry)
-    title = parse_title(latest_entry, type)
-    description = parse_description(latest_entry, type)
-    media = parse_media_links(latest_entry)
-    latest_message_id = get_latest_message_id(chat_id, type, name)
-    await send_update(bot, chat_id, type, name, link, title, description, latest_message_id, media)
+    latest_message_id = await send_update(
+        bot,
+        chat_id,
+        type,
+        name,
+        parse_link(latest_entry),
+        parse_title(latest_entry, type),
+        parse_description(latest_entry, type),
+        get_latest_message_id(chat_id, type, name),
+        parse_media_links(latest_entry),
+    )
+    update_latest_message_id(chat_id, type, name, latest_message_id)
