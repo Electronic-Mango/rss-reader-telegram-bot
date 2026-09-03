f956204b9d22d18bc9298c10ae26aa211ef3e79c
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-09-03 21:49:26 +0200

    Reduce the time needed to stop the bot
    Add custom handlers for SIGINT, SIGTERM, SIGABRT, which force the current update loop to stop.
    Otherwise, PTB waits for the job to finish, which might take a while.

--- src/main/bot/telegram_bot.py
+++ src/main/bot/telegram_bot.py
@@ -7,6 +7,9 @@ Main bot module, which is responsible for:
 All these actions are triggered by a single function.
 """
 
+from asyncio import get_running_loop
+from signal import SIGABRT, SIGINT, SIGTERM
+
 from loguru import logger
 from telegram.ext import Application, ApplicationBuilder, Defaults, JobQueue, PicklePersistence
 
@@ -17,7 +20,7 @@ from bot.command.remove_all import remove_all_followup_handler, remove_all_initi
 from bot.command.start_help import start_help_command_handler
 from bot.command.subs.handler import subscriptions_followup_handler, subscriptions_initial_handler
 from bot.error_handler import handle_errors
-from bot.update_checker import check_for_all_updates
+from bot.update_checker import cancel_active_update_check, check_for_all_updates
 from db.client import initialize_db
 from settings import LOOKUP_INITIAL_DELAY, LOOKUP_INTERVAL, PERSISTENCE_FILE, TOKEN
 
@@ -38,7 +41,7 @@ def run_bot() -> None:
     application = _prepare_application()
     _configure_handlers(application)
     _start_checking_for_updates(application.job_queue)
-    application.run_polling()
+    application.run_polling(stop_signals=None)
 
 
 def _prepare_application() -> Application:
@@ -48,9 +51,22 @@ def _prepare_application() -> Application:
         .defaults(Defaults("HTML"))
         .arbitrary_callback_data(True)
         .persistence(PicklePersistence(PERSISTENCE_FILE))
-        .post_init(lambda _: initialize_db())
+        .post_init(_post_init)
         .build()
     )
+
+
+async def _post_init(application: Application) -> None:
+    await initialize_db()
+    event_loop = get_running_loop()
+    for stop_signal in (SIGINT, SIGTERM, SIGABRT):
+        event_loop.add_signal_handler(stop_signal, _handle_shutdown_signal, application)
+
+
+def _handle_shutdown_signal(application: Application) -> None:
+    logger.info("Shutdown signal received")
+    cancel_active_update_check()
+    application.stop_running()
 
 
 def _configure_handlers(application: Application) -> None:
