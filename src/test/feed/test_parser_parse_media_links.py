440a1479b10a817b221cfb4e3ad7c54ac72006f6
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-09-12 15:29:08 +0200

    Update unit tests for parser.py

--- src/test/feed/test_parser_parse_media_links.py
+++ src/test/feed/test_parser_parse_media_links.py
@@ -1,7 +1,31 @@
+from unittest.mock import patch
+
 from feedparser import FeedParserDict
 from pytest import mark
 
 from feed.parser import parse_media_links
+from settings import Settings
+
+FEED_NAME = "test-feed-name"
+
+FEED_TYPE_NO_OVERRIDE = "test-feed-type-no-override"
+FEED_TYPE_OVERRIDE = "test-feed-type"
+FEED_TYPE_OVERRIDE_PARAMETRIZED = "test-feed-type-parametrized-override"
+ENTRY_ID = "test-entry-id"
+ENTRY_LINK = "test-entry-link"
+OVERRIDE_PATTERN_NO_PARAMETERS = "override-pattern"
+OVERRIDE_PATTERN = "{feed_type}_{feed_name}_{entry_id}_{entry_link}"
+OVERRIDE_VALUE = OVERRIDE_PATTERN.format(
+    feed_type=FEED_TYPE_OVERRIDE_PARAMETRIZED,
+    feed_name=FEED_NAME,
+    entry_id=ENTRY_ID,
+    entry_link=ENTRY_LINK,
+)
+RSS_FEEDS = {
+    FEED_TYPE_OVERRIDE: {"media_override": OVERRIDE_PATTERN_NO_PARAMETERS},
+    FEED_TYPE_OVERRIDE_PARAMETRIZED: {"media_override": OVERRIDE_PATTERN},
+    FEED_TYPE_NO_OVERRIDE: {"show_description": True},
+}
 
 ENTRY_WITH_MEDIA_CONTENT = FeedParserDict(
     {"media_content": [{"url": "link-1"}, {"url": "link-2"}]}
@@ -16,13 +40,43 @@ MEDIA_LINKS_IN_SUMMARY = """
 ENTRY_WITHOUT_MEDIA_CONTENT = FeedParserDict({"summary": MEDIA_LINKS_IN_SUMMARY})
 EXPECTED_LINKS_FROM_SUMMARY = ["expected-img-link", "expected-source-link"]
 
+ENTRY_FOR_PARAMETRIZED_OVERRIDE = FeedParserDict(
+    {
+        "media_content": [{"url": "link-1"}, {"url": "link-2"}],
+        "summary": MEDIA_LINKS_IN_SUMMARY,
+        "id": ENTRY_ID,
+        "link": ENTRY_LINK,
+    }
+)
+
 
 @mark.parametrize(
-    argnames=("entry", "expected_links"),
+    argnames=("feed_type", "entry", "expected_links"),
     argvalues=[
-        (ENTRY_WITH_MEDIA_CONTENT, EXPECTED_LINKS_FROM_MEDIA_CONTENT),
-        (ENTRY_WITHOUT_MEDIA_CONTENT, EXPECTED_LINKS_FROM_SUMMARY),
+        (
+            FEED_TYPE_NO_OVERRIDE,
+            ENTRY_WITH_MEDIA_CONTENT,
+            EXPECTED_LINKS_FROM_MEDIA_CONTENT,
+        ),
+        (
+            FEED_TYPE_NO_OVERRIDE,
+            ENTRY_WITHOUT_MEDIA_CONTENT,
+            EXPECTED_LINKS_FROM_SUMMARY,
+        ),
+        (
+            FEED_TYPE_OVERRIDE,
+            ENTRY_WITH_MEDIA_CONTENT,
+            [OVERRIDE_PATTERN_NO_PARAMETERS],
+        ),
+        (
+            FEED_TYPE_OVERRIDE_PARAMETRIZED,
+            ENTRY_FOR_PARAMETRIZED_OVERRIDE,
+            [OVERRIDE_VALUE],
+        ),
     ],
 )
-def test_parse_media_links(entry: FeedParserDict, expected_links: list[str]) -> None:
-    assert expected_links == parse_media_links(entry)
+@patch.object(Settings, "RSS_FEEDS", RSS_FEEDS)
+def test_parse_media_links(
+    feed_type: str, entry: FeedParserDict, expected_links: list[str]
+) -> None:
+    assert expected_links == parse_media_links(entry, feed_type, FEED_NAME)
