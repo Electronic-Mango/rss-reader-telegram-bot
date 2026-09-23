7eb511782e441f8122200bb7274792dc8353bf6d
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-09-23 13:56:49 +0200

    Skip latest: update get_latest_data unit tests

--- src/test/feed/test_reader_get_latest_data.py
+++ src/test/feed/test_reader_get_latest_data.py
@@ -4,6 +4,7 @@ from pytest import mark
 
 from feed.reader import get_latest_data
 from feed.types import RssEntry
+from settings import Settings
 
 FEED_LINK = "FEED_LINK"
 
@@ -13,9 +14,10 @@ ENTRIES = [
     {"published_parsed": strptime("04.04.2004", "%d.%m.%Y")},
     {"published_parsed": strptime("03.03.2003", "%d.%m.%Y")},
 ]
+
 EXPECTED_LATEST_ID = "LATEST_ID"
 EXPECTED_LATEST_LINK = "LATEST_LINK"
-EXPECTED_LATEST_DATE = strptime("05.05.2005", "%d.%m.%Y")
+EXPECTED_LATEST_DATE = strptime("06.06.2006", "%d.%m.%Y")
 EXPECTED_LATEST_ENTRY_DATA = (
     EXPECTED_LATEST_ID,
     EXPECTED_LATEST_LINK,
@@ -27,15 +29,47 @@ LATEST_ENTRY = {
     "published_parsed": EXPECTED_LATEST_DATE,
 }
 
+EXPECTED_SECOND_LATEST_ID = "SECOND_LATEST_ID"
+EXPECTED_SECOND_LATEST_LINK = "SECOND_LATEST_LINK"
+EXPECTED_SECOND_LATEST_DATE = strptime("05.05.2005", "%d.%m.%Y")
+EXPECTED_SECOND_LATEST_ENTRY_DATA = (
+    EXPECTED_SECOND_LATEST_ID,
+    EXPECTED_SECOND_LATEST_LINK,
+    EXPECTED_SECOND_LATEST_DATE,
+)
+SECOND_LATEST_ENTRY = {
+    "id": EXPECTED_SECOND_LATEST_ID,
+    "link": EXPECTED_SECOND_LATEST_LINK,
+    "published_parsed": EXPECTED_SECOND_LATEST_DATE,
+}
+
 
 @mark.parametrize(
     argnames="entries",
     argvalues=[
-        [*ENTRIES, LATEST_ENTRY],
-        [LATEST_ENTRY, *ENTRIES],
-        [*ENTRIES[:2], LATEST_ENTRY, *ENTRIES[2:]],
+        [*ENTRIES, SECOND_LATEST_ENTRY, LATEST_ENTRY],
+        [*ENTRIES, LATEST_ENTRY, SECOND_LATEST_ENTRY],
+        [LATEST_ENTRY, SECOND_LATEST_ENTRY, *ENTRIES],
+        [SECOND_LATEST_ENTRY, LATEST_ENTRY, *ENTRIES],
+        [*ENTRIES[:2], LATEST_ENTRY, SECOND_LATEST_ENTRY, *ENTRIES[2:]],
+        [*ENTRIES[:2], SECOND_LATEST_ENTRY, LATEST_ENTRY, *ENTRIES[2:]],
     ],
 )
 def test_get_latest_data(entries: list[RssEntry]) -> None:
     feed = {"href": FEED_LINK, "entries": entries}
+
+    Settings.SKIP_LATEST_ON_ADD = False
+    assert get_latest_data(feed) == EXPECTED_LATEST_ENTRY_DATA
+
+    Settings.SKIP_LATEST_ON_ADD = True
+    assert get_latest_data(feed) == EXPECTED_SECOND_LATEST_ENTRY_DATA
+
+
+def test_get_latest_data_selects_latest_on_feeds_with_one_entry() -> None:
+    feed = {"href": FEED_LINK, "entries": [LATEST_ENTRY]}
+
+    Settings.SKIP_LATEST_ON_ADD = False
+    assert get_latest_data(feed) == EXPECTED_LATEST_ENTRY_DATA
+
+    Settings.SKIP_LATEST_ON_ADD = True
     assert get_latest_data(feed) == EXPECTED_LATEST_ENTRY_DATA
